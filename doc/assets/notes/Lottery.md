f497ed3815fd83906561369ff0bf422f6ff409c9
Neil Nan <2378428641@qq.com>
2023-05-06 14:01:16 +0800

    230506_neil_buildFramework

--- doc/assets/notes/Lottery.md
+++ doc/assets/notes/Lottery.md
@@ -32,5 +32,43 @@
 
 # 第 2 部分 领域开发
 
+## 第01节：环境、配置、规范
 
+## 第02节：搭建DDD四层架构
+
+> - 分支：230504_neil_initProject
+> - 目的：搭建工程结构
+
+**DDD分层架构介绍**
+
+![2-02](E:\Document\Typora笔记\Pictures\2-02.png)
+
+- 应用层{application}
+- 领域层{domain}
+- 基础层{infrastructure}
+- 接口层{interfaces}
+
+**DDD + RPC，模块分离系统搭建**
+
+![2-03](E:\Document\Typora笔记\Pictures\2-03.png)
+
+**为什么要把RPC层单独拆分出来**
+
+使用 RPC 框架的时候，需要对外提供描述接口信息的 Jar 让外部调用方引入才可以通过反射调用到具体的方法提供者，那么这个时候，RPC 需要暴露出来，而 DDD 的系统结构又比较耦合，怎么进行模块化的分离就成了问题点。
+
+### 2.2 项目实现
+
+1. 通过git创建新的项目，使用ssh或者用户密码登录将项目clone到本地
+
+   - 检查Git是否已经有SSH密钥，没有进行生成[(104条消息) Git 使用 —设置 Git 的SSH秘钥_git配置ssh密钥_嵌入式之入坑笔记的博客-CSDN博客](https://blog.csdn.net/weixin_43866583/article/details/125680653)
+
+   - 将密钥添加到对应的gitcode中
+
+     ![image-20230505142103597](E:\Document\Typora笔记\Pictures\image-20230505142103597.png)
+
+2. 按照对应结构创建对应的module
+
+   ![image-20230505142229618](E:\Document\Typora笔记\Pictures\image-20230505142229618.png)
+
+   
 
--- lottery-interfaces/src/test/java/cn/itedus/lottery/test/ApiTest.java
+++ lottery-interfaces/src/test/java/cn/itedus/lottery/test/ApiTest.java
@@ -1,4 +1,44 @@
 package cn.itedus.lottery.test;
 
+import cn.itedus.lottery.infrastructure.dao.IActivityDao;
+import cn.itedus.lottery.infrastructure.po.Activity;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JsonbTester;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ApiTest {
+
+    @Resource
+    IActivityDao activityDao;
+
+    @Test
+    public void test_insert(){
+        Activity activity = new Activity();
+        activity.setActivityId(100001L);
+        activity.setActivityName("测试活动");
+        activity.setActivityDesc("仅用于插入数据测试");
+        activity.setBeginDateTime(new Date());
+        activity.setEndDateTime(new Date());
+        activity.setStockCount(100);
+        activity.setTakeCount(10);
+        activity.setState(0);
+        activity.setCreator("neilnan");
+        activityDao.insert(activity);
+    }
+
+    @Test
+    public void test_select(){
+        Activity activity = activityDao.queryActivityById(100001L);
+        log.info("测试结果：{}", JSON.toJSONString(activity));
+    }
 }
